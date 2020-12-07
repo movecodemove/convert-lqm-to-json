9b2d2637d6f184291f84e5c3964f6bc03671b512
Scott Steven Rodriguez <scott@movecodemove.com>
2020-12-07 02:28:33 -0500

    Using poetry for dependency management and development scripting More robust input/output options and usage information Ready to publish to Pypi

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
-### Convert LG QuickMemo+ export into single, text-only JSON file
+# Convert LG QuickMemo+ files into single, text-only JSON file
 
-```sh
-$ python convert-lqm-to-json.py <path to directory that contains .lqm files>
-```
+## Installation
+`$ pip install [--user] convert-lqm-to-json`
+
+## Usage
+`$ convert-lqm-to-json [-h] [-o [<...>]] [-v] [<source1>] ... [<sourceN>]`
+
+#### Arguments
+
+    <source>                Paths or glob patterns for resolving .lqm files;
+                            defaults to current directory.
+
+#### Options
+
+    -o  (--output-dir)      Output directory; defaults to current directory.
+    -h  (--help)            Print help message.
+    -v  (--version)         Print application version.
---
+++ scripts.py
@@ -0,0 +1,44 @@
+from subprocess import PIPE, run
+
+
+def _bump_version():
+    current_version = _run_and_capture(["poetry", "version", "-s"])
+
+    run(["poetry", "version", "patch", "-s"])
+
+    new_version = _run_and_capture(["poetry", "version", "-s"])
+
+    for path in ["convert_lqm_to_json/__init__.py", "tests/test___init__.py"]:
+        _replace_in_file(path, current_version, new_version)
+
+
+def _replace_in_file(file_path, current_string, new_string):
+    with open(file_path, "r+") as file:
+        text = file.read()
+        file.seek(0)
+        file.write(text.replace(current_string, new_string))
+        file.truncate()
+
+
+def _run_and_capture(args):
+    return run(args, stdout=PIPE, text=True).stdout.strip()
+
+
+def build():
+    run(["rm", "-rf", "dist"])
+    run(["poetry", "build"])
+
+
+def format():
+    run(["black", "convert_lqm_to_json/", "tests/"])
+
+
+def publish():
+    test()
+    _bump_version()
+    build()
+    run(["poetry", "publish", "-r", "testpypi"])
+
+
+def test():
+    run(["pytest", "tests"])
