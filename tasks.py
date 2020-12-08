72c062ba48b3fa2363410f970514ebad566c15d7
Scott Steven Rodriguez <scott@movecodemove.com>
2020-12-07 23:45:41 -0500

    Added some error handling to tasks Removed Python 3.9 and added 3.7 to Github CI testing

--- tasks.py
+++ tasks.py
@@ -2,11 +2,11 @@ from subprocess import PIPE, run
 
 
 def _bump_version():
-    current_version = _run_and_capture(["poetry", "version", "-s"])
+    current_version = _run_command(["poetry", "version", "-s"], capture_output=True)
 
-    run(["poetry", "version", "patch", "-s"])
+    _run_command(["poetry", "version", "patch", "-s"])
 
-    new_version = _run_and_capture(["poetry", "version", "-s"])
+    new_version = _run_command(["poetry", "version", "-s"], capture_output=True)
 
     for path in ["convert_lqm_to_json/__init__.py", "tests/test___init__.py"]:
         _replace_in_file(path, current_version, new_version)
@@ -22,27 +22,38 @@ def _replace_in_file(file_path, current_string, new_string):
         file.truncate()
 
 
-def _run_and_capture(args):
-    return run(args, stdout=PIPE, text=True).stdout.strip()
+def _run_command(args, capture_output=False):
+    result = run(args, capture_output, text=True if capture_output else False)
+
+    if result.returncode == 0:
+        if capture_output:
+            return result.stdout.strip()
+
+    else:
+        if capture_output:
+            print(
+                result.stderr.strip() or f"Error encountered running {arg[0]} command."
+            )
+
+        exit(result.returncode)
 
 
 def build():
-    run(["rm", "-rf", "dist"])
-    run(["poetry", "build"])
+    _run_command(["rm", "-rf", "dist"])
+    _run_command(["poetry", "build"])
 
 
 def commit():
-    run(["git", "add", "--all"])
-    run(["git", "commit"])
+    _run_command(["git", "add", "--all"])
+    _run_command(["git", "commit"])
 
 
 def publish():
-    test()
     version = _bump_version()
     commit()
-    run(["git", "tag", "-a", f"v{version}", "-m", f"Version {version}"])
-    run(["git", "push", "--follow-tags"])
+    _run_command(["git", "tag", "-a", f"v{version}", "-m", f"Version {version}"])
+    _run_command(["git", "push", "--follow-tags"])
 
 
 def test():
-    run(["pytest", "tests"])
+    _run_command(["pytest", "tests"])
